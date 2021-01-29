2ad22c994ab09de2fbbaee49e90909aec5b2c82f
Ewoud Lindemans <e.lindemans@outlook.com>
2021-01-30 00:11:42 +0100

    Beautifull

--- scripts/radugen.js
+++ scripts/radugen.js
@@ -15,33 +15,11 @@ Hooks.on("ready", function () {
     }
 });
 
-// const dungeon_names = ["Vault", "Catacombs", "Dungeon", "Palace", "Eternal"];
-// const curiosities = ["Endless Suffering", "Agony Burrows", "Dream Cells", "Mystic", "Back Itches", "Paper Cuts"];
-
-// function getRndFromArr(arr) {
-//     return arr[Math.floor(Math.random() * arr.length)];
-// }
-
-// function uuidv4() {
-//     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-//         let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-//         return v.toString(16);
-//     });
-// }
-
 Hooks.on("chatMessage", function (_, message) {
     if (message.indexOf('/radugen') != 0) return;
 
-    // var id = uuidv4();
     const rdg_scene = new radugen.customScene();
-
     radugen.compendium.scene.importEntity(rdg_scene);
-    console.log(rdg_scene._id);
-
-    game.scenes.entities[rdg_scene._id] = rdg_scene;
-    rdg_scene.activate().then(function () {
-
-    });
 
     return false;
 });
