5a55331abec12b99f9b15ee37e004ee642aca51b
Stefan Wouters-Yaowanasap <mail@sw1.nl>
2021-01-30 02:31:51 +0100

    Added generate parameters for width/height

--- scripts/customScene.js
+++ scripts/customScene.js
@@ -6,11 +6,11 @@ radugen.customScene = class extends Scene {
      * @param {number} height
      * @param {number} tilesize of 64 128 192 256 ...
      */
-    constructor(img) {
+    constructor(width, height, img) {
         const dungeon_names = ["Vault", "Catacombs", "Dungeon", "Palace", "Eternal", "Repository", "Lair"];
         const curiosities = ["Endless Suffering", "Agony Burrows", "Dream Cells", "Mystic", "Back Itches", "Paper Cuts", "Random Bugs", "Calories"];
 
-        let grid = [20, 20];
+        let grid = [width, height];
         let size = 256;
 
         super({
@@ -25,34 +25,32 @@ radugen.customScene = class extends Scene {
             tiles: [],
             img: img,
         });
+
+        this._width = width;
+        this._height = height;
     }
 
-    static getImage(imageloaded) {
-        let grid = [20, 20];
+    static getImage(imageloaded, width, height) {
+        let grid = [width, height];
         let size = 256;
 
-        var vancas = document.createElement('canvas');
+        const vancas = document.createElement('canvas');
         vancas.width = grid[0] * size;
         vancas.height = grid[1] * size;
 
-        
-
-        let ctx = vancas.getContext("2d");
+        const ctx = vancas.getContext("2d");
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, vancas.width, vancas.height);
  
         let imgcount = 0;
-        console.log(grid[0] * grid[1]);
-        for (var x = 0; x < grid[0]; x++) {
-            for (var y = 0; y < grid[1]; y++) {
+        for (let x = 0; x < grid[0]; x++) {
+            for (let y = 0; y < grid[1]; y++) {
                 ((x, y) => {
                     //Get random image
-                    var img = new Image();
+                    const img = new Image();
                     img.onload = function () {
-                        
-
                         //Rotate
-                        var r = radugen.helper.getRndFromNum(4)
+                        let r = radugen.helper.getRndFromNum(4)
 
                         ctx.translate((x * size) + size / 2, (y * size) + size / 2);
                         ctx.rotate((Math.PI / 2) * r);
@@ -64,8 +62,6 @@ radugen.customScene = class extends Scene {
                         ctx.setTransform(1, 0, 0, 1, 0, 0)
 
                         imgcount++;
-                        console.log(imgcount);
-                        console.log(x * size, y * size);
                         if (imgcount == grid[0] * grid[1]) {
                             imageloaded.bind(vancas.toDataURL())();
                         }
@@ -76,5 +72,19 @@ radugen.customScene = class extends Scene {
         }
 
         
+    }
+
+    /**
+     * @type {number}
+     */
+    get width(){
+        return this._width;
+    }
+
+    /**
+     * @type {number}
+     */
+    get height(){
+        return this._height;
     }
 };
