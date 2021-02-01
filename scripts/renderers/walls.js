3c08064b01d4e26bd9c7ba3fbbb5e59e602b50a1
Ewoud Lindemans <e.lindemans@outlook.com>
2021-02-01 04:54:11 +0100

    Background texture, Fixed context reset, Added support for extra wall-mode (prettier but less "rule like")

--- scripts/renderers/walls.js
+++ scripts/renderers/walls.js
@@ -65,11 +65,33 @@ radugen.renderer.Walls = class {
         }
     }
 
+    getInvisibleWall(x, y, direction) {
+        return {
+            c: this.getWallForSide(x, y, direction),
+            move: CONST.WALL_MOVEMENT_TYPES.NORMAL,
+            sense: CONST.WALL_SENSE_TYPES.NONE,
+            dir: CONST.WALL_DIRECTIONS.BOTH,
+            door: CONST.WALL_DOOR_TYPES.NONE,
+            ds: CONST.WALL_DOOR_STATES.CLOSED
+        }
+    }
+
     getWall(x, y, direction) {
         return {
             c: this.getWallForSide(x, y, direction),
             move: CONST.WALL_MOVEMENT_TYPES.NORMAL,
             sense: CONST.WALL_SENSE_TYPES.NORMAL,
+            dir: CONST.WALL_DIRECTIONS.BOTH,
+            door: CONST.WALL_DOOR_TYPES.NONE,
+            ds: CONST.WALL_DOOR_STATES.CLOSED
+        }
+    }
+
+    getTerrainWall(x, y, direction) {
+        return {
+            c: this.getWallForSide(x, y, direction),
+            move: CONST.WALL_MOVEMENT_TYPES.NORMAL,
+            sense: CONST.WALL_SENSE_TYPES.LIMITED,
             dir: CONST.WALL_DIRECTIONS.BOTH,
             door: CONST.WALL_DOOR_TYPES.NONE,
             ds: CONST.WALL_DOOR_STATES.CLOSED
@@ -86,31 +108,103 @@ radugen.renderer.Walls = class {
         const walls = [];
         this.iterateMap(function (x, y) {
             if (self._map[x][y] == 0) {
+                let drawTop = false;
+                let drawLeft = false;
+                let drawRight = false;
+                let drawBottom = false;
+                let drawCount = 0;
+
                 //check top
                 if (y != 0) {
                     if (self._map[x][y - 1] != 0) {
-                        walls.push(self.getWall(x, y, 'top'));
+                        drawTop = true;
+                        drawCount++;
                     }
                 }
 
                 //check left
                 if (x != 0) {
                     if (self._map[x - 1][y] != 0) {
-                        walls.push(self.getWall(x, y, 'left'));
+                        drawLeft = true;
+                        drawCount++;
                     }
                 }
                
                 //check right
                 if (x != self._gridWidth - 1) {
                     if (self._map[x + 1][y] != 0) {
-                        walls.push(self.getWall(x, y, 'right'));
+                        drawRight = true;
+                        drawCount++;
                     }
                 }
 
                 //check bottom
                 if (y != self._gridHeight - 1) {
                     if (self._map[x][y + 1] != 0) {
+                        drawBottom = true;
+                        drawCount++;
+                    }
+                }
+
+                if (drawTop) {
+                    walls.push(self.getInvisibleWall(x, y, 'top'));
+                }
+                if (drawLeft) {
+                    walls.push(self.getInvisibleWall(x, y, 'left'));
+                }
+                if (drawRight) {
+                    walls.push(self.getInvisibleWall(x, y, 'right'));
+                }
+                if (drawBottom) {
+                    walls.push(self.getInvisibleWall(x, y, 'bottom'));
+                }
+
+                if (drawCount == 1) {
+                    if (drawTop) {
                         walls.push(self.getWall(x, y, 'bottom'));
+                    }
+                    if (drawLeft) {
+                        walls.push(self.getWall(x, y, 'right'));
+                    }
+                    if (drawRight) {
+                        walls.push(self.getWall(x, y, 'left'));
+                    }
+                    if (drawBottom) {
+                        walls.push(self.getWall(x, y, 'top'));
+                    }
+                }
+
+                if (drawCount == 0) {
+                    //check top left
+                    if (y != 0 && x != 0) {
+                        if (self._map[x - 1][y - 1] != 0) {
+                            walls.push(self.getWall(x, y, 'right'));
+                            walls.push(self.getWall(x, y, 'bottom'));
+                        }
+                    }
+
+                    //check top right
+                    if (y != 0 && x != self._gridWidth - 1) {
+                        if (self._map[x + 1][y - 1] != 0) {
+                            walls.push(self.getWall(x, y, 'left'));
+                            walls.push(self.getWall(x, y, 'bottom'));
+                        }
+                    }
+
+                    //check bottom left
+                    if (y != self._gridHeight - 1 && x != 0) {
+                        if (self._map[x - 1][y + 1] != 0) {
+                            walls.push(self.getWall(x, y, 'right'));
+                            walls.push(self.getWall(x, y, 'top'));
+                        }
+                    }
+
+                    //check bottom right
+                    if (y != self._gridHeight - 1 && x != self._gridWidth - 1) {
+                        if (self._map[x + 1][y + 1] != 0) {
+                            walls.push(self.getWall(x, y, 'left'));
+                            walls.push(self.getWall(x, y, 'top'));
+                        }
                     }
                 }
             };
