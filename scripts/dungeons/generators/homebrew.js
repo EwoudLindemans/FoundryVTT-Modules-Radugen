3e4f9e2629fbc81f52f9e82f8b594ded6cf2b014
Ewoud Lindemans <e.lindemans@outlook.com>
2021-03-01 01:23:00 +0100

    Oops i did it again

--- scripts/dungeons/generators/homebrew.js
+++ scripts/dungeons/generators/homebrew.js
@@ -37,21 +37,22 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
                 }
             }
             // if(rasterizedGrid[y][x].type == TileType.Corridor){
+            //     grid[y][x] = countDracula;
             //     winners.push([x, y, countDracula]);
             //     return true;
             // }
 
             // Check if we have to evade this tile
-            if(rasterizedGrid[y][x].type == TileType.Room && countDracula != 0){
+            if(rasterizedGrid[y][x].room != null && countDracula != 0){
                 return;
             } 
 
-            // if(rasterizedGrid[y][x].type == TileType.Void){
-            //     let tile = new Tile(x + offsetX, y + offsetY, TileType.Corridor);
-            //     tile.debug = `rgba(254,0,${countDracula * 30}, 1)`;
-            //     tile.debugInfo = countDracula;
-            //     this._grid.push(tile)
-            // }
+            if(rasterizedGrid[y][x].type == TileType.Void){
+                let tile = new Tile(x + offsetX, y + offsetY, TileType.Corridor);
+                tile.debug = `rgba(${254 - countDracula * 30},${countDracula * 30},0, 1)`;
+                tile.debugInfo = countDracula;
+                this._grid.push(tile)
+            }
 
             grid[y][x] = countDracula;
             const diagonal = false;
@@ -72,9 +73,16 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
             }
         }
 
+        
+
         //Start dracula
         for (let startPoint of startPoints){ dracula(startPoint.x - offsetX, startPoint.y - offsetY, vladzerizedGrid, 0); };
         for(let i = 0; i < iterateStack.length; i++){ if(iterateStack[i]() == true){ break; } }
+
+        if(winners.length == 0){
+            console.warn(`unresolved path for`, startPoints)
+            
+        }
 
         iterateStack = [];
         const alucard = (x, y, grid, countAlucard, start) => {
@@ -89,23 +97,22 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
                 }
 
                 const diagonal = false;
-                //Go to a random count thats lower than the current
                 let randomOffset = rnd(4) - 1;
                 randomOffset = randomOffset * 90;
-                for (let a = 0; a < 360; a += diagonal ? 45 : 90) {
+                for (let a = randomOffset; a < 360 + randomOffset; a += diagonal ? 45 : 90) {
                     const rad = a * Math.PI / 180;
                     const [xx, yy] = [Math.round(Math.sin(rad)), Math.round(Math.cos(rad))];
 
                     if (x + xx < gridMinX || y + yy < gridMinY || x + xx > gridMaxX || y + yy > gridMaxY) { continue; }
 
                     // //Make sure we dont hit walls
-                    // if (rasterizedGrid[y][x].wall.left && xx == -1 ||
-                    //     rasterizedGrid[y][x].wall.top && yy == -1 ||
-                    //     rasterizedGrid[y][x].wall.right && xx == 1 ||
-                    //     rasterizedGrid[y][x].wall.bottom && yy == 1) 
-                    // {
-                    //     continue;
-                    // }
+                    if (rasterizedGrid[y][x].wall.left && xx == -1 ||
+                        rasterizedGrid[y][x].wall.top && yy == -1 ||
+                        rasterizedGrid[y][x].wall.right && xx == 1 ||
+                        rasterizedGrid[y][x].wall.bottom && yy == 1) 
+                    {
+                        continue;
+                    }
 
                     if (grid[y + yy][x + xx] == countAlucard - 1) {
                         iterateStack.push(() => alucard(x + xx, y + yy, grid, countAlucard - 1));
@@ -117,6 +124,7 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
 
         //Start alucard
         for(let i = 0; i < winners.length; i++){ //Check if we have a winning condition
+            console.log(winners);
             alucard(winners[i][0], winners[i][1], vladzerizedGrid, winners[i][2], winners[i][2]);
         }
         for(let i = 0; i < iterateStack.length; i++){ //Iterate the grid
@@ -146,6 +154,112 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
         });
     }
     
+    findCommonWalls(room1, room2, invertAxis){
+        const rnd = radugen.helper.getRndFromArr;
+        let debug = false;
+        let found = false;
+        let room1Info = this.getSize(room1.tiles);
+        let room2Info = this.getSize(room2.tiles);
+
+        if(room1Info.left == room2Info.right - 1 || room2Info.left == room1Info.right + 1){
+            let yRangeRoom1 = Array.from({length: room1Info.height}).map((_, x) => room1Info.top + x);
+            let yRangeRoom2 = Array.from({length: room2Info.height}).map((_, x) => room2Info.top + x);
+            let sharedYRange = [];
+
+            for(let yRoom1 of yRangeRoom1){
+                for(let yRoom2 of yRangeRoom2){
+                    if(yRoom1 == yRoom2){
+                        sharedYRange.push(yRoom1);
+                    }
+                }
+            }
+
+            if (sharedYRange.length) {
+                let randomShared = rnd(sharedYRange);
+                if (invertAxis) {
+                    let room1Tile = room1.tiles.find(tile => tile.x == room1Info.left && tile.y == randomShared);
+                    let room2Tile = room2.tiles.find(tile => tile.x == room2Info.right && tile.y == randomShared)
+
+                    if (debug) {
+                        room2Tile.debug = 'green';
+                        room1Tile.debug = 'purple';
+                    }
+                    if (!found) {
+                        room1Tile.wall.left = false;
+                        room1Tile.passage.left = true;
+                        room2Tile.wall.right = false;
+                        room2Tile.passage.right = true;
+                    }
+                }
+                else {
+                    let room1Tile = room1.tiles.find(tile => tile.x == room1Info.right && tile.y == randomShared);
+                    let room2Tile = room2.tiles.find(tile => tile.x == room2Info.left && tile.y == randomShared)
+
+                    if (debug) {
+                        room2Tile.debug = 'orange';
+                        room1Tile.debug = 'blue';
+                    }
+                    if (!found) {
+                        room1Tile.wall.right = false;
+                        room1Tile.passage.right = true;
+                        room2Tile.wall.left = false;
+                        room2Tile.passage.left = true;
+                    }
+                }
+                found = true;
+            }
+        }
+
+        if (room1Info.top == room2Info.bottom - 1 || room2Info.top == room1Info.bottom + 1) {
+            let xRangeRoom1 = Array.from({ length: room1Info.width }).map((_, x) => room1Info.left + x);
+            let xRangeRoom2 = Array.from({ length: room2Info.width }).map((_, x) => room2Info.left + x);
+            let sharedXRange = [];
+            for (let xRoom1 of xRangeRoom1) {
+                for (let xRoom2 of xRangeRoom2) {
+                    if (xRoom1 == xRoom2) {
+                        sharedXRange.push(xRoom1);
+                    }
+                }
+            }
+
+            if(sharedXRange.length){
+                let randomShared = rnd(sharedXRange);
+                if (invertAxis) {
+                    let room1Tile = room1.tiles.find(tile => tile.x == randomShared && tile.y == room1Info.bottom);
+                    let room2Tile = room2.tiles.find(tile => tile.x == randomShared && tile.y == room2Info.top);
+
+                    if (debug) {
+                        room2Tile.debug = 'blue';
+                        room1Tile.debug = 'orange';
+                    }
+                    if (!found) {
+                        room1Tile.wall.bottom = false;
+                        room1Tile.passage.bottom = true;
+                        room2Tile.wall.top = false;
+                        room2Tile.passage.top = true;
+                    }
+                }
+                else {
+                    let room1Tile = room1.tiles.find(tile => tile.x == randomShared && tile.y == room1Info.top);
+                    let room2Tile = room2.tiles.find(tile => tile.x == randomShared && tile.y == room2Info.bottom);
+
+                    if (debug) {
+                        room2Tile.debug = 'blue';
+                        room1Tile.debug = 'orange';
+                    }
+                    if (!found) {
+                        room1Tile.wall.bottom = false;
+                        room1Tile.passage.bottom = true;
+                        room2Tile.wall.top = false;
+                        room2Tile.passage.top = true;
+                    }
+                }
+                found = true;
+            }
+        }
+        return found;
+    }
+
     createEntrance(room1, room2, invertAxis){
         const [rnd] = [radugen.helper.getRndFromNum];
 
@@ -157,45 +271,46 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
         let y1 = invertAxis ? room2Info.top : room2Info.top + rnd(room2Info.height - 1);
         let y2 = invertAxis ? room1Info.bottom + 1 : room1Info.top + rnd(room1Info.height - 1);
 
-        //TODO: if walls bang, make corridor
-
-        //Register tiles as entrance on the room
-        for (let tile of room2.tiles) {
-            if (tile.x == x1 && tile.y == y1) {
-                if (invertAxis) {
-                    tile.room.connections += 1;
-                    tile.debug = 'red';
-                    tile.disallowObjectPlacement = true;
-                    tile.wall.top = false;
-                    tile.passage.top = true;
-                    tile.passage.target = room1.index;
-                }
-                else {
-                    tile.room.connections += 1;
-                    tile.debug = 'orange';
-                    tile.disallowObjectPlacement = true;
-                    tile.wall.left = false;
-                    tile.passage.left = true;
-                    tile.passage.target = room1.index;
+        
+        if (!this.findCommonWalls(room1, room2, invertAxis)) {
+            //Register tiles as entrance on the room
+            for (let tile of room2.tiles) {
+                if (tile.x == x1 && tile.y == y1) {
+                    if (invertAxis) {
+                        tile.room.connections += 1;
+                        tile.debug = 'red';
+                        tile.disallowObjectPlacement = true;
+                        tile.wall.top = false;
+                        tile.passage.top = true;
+                        tile.passage.target = room1.index;
+                    }
+                    else {
+                        tile.room.connections += 1;
+                        tile.debug = 'orange';
+                        tile.disallowObjectPlacement = true;
+                        tile.wall.left = false;
+                        tile.passage.left = true;
+                        tile.passage.target = room1.index;
+                    }
                 }
             }
-        }
-        for (let tile of room1.tiles) {
-            if (tile.x == x2 && tile.y == y2 - 1) {
-                tile.room.connections += 1;
-                tile.debug = 'blue';
-                tile.disallowObjectPlacement = true;
-                tile.wall.bottom = false;
-                tile.passage.bottom = true;
-                tile.passage.target = room2.index;
-            }
-            if (tile.x == x2 - 1 && tile.y == y2) {
-                tile.room.connections += 1;
-                tile.debug = 'green';
-                tile.disallowObjectPlacement = true;
-                tile.wall.right = false;
-                tile.passage.right = true;
-                tile.passage.target = room2.index;
+            for (let tile of room1.tiles) {
+                if (tile.x == x2 && tile.y == y2 - 1) {
+                    tile.room.connections += 1;
+                    tile.debug = 'blue';
+                    tile.disallowObjectPlacement = true;
+                    tile.wall.bottom = false;
+                    tile.passage.bottom = true;
+                    tile.passage.target = room2.index;
+                }
+                if (tile.x == x2 - 1 && tile.y == y2) {
+                    tile.room.connections += 1;
+                    tile.debug = 'green';
+                    tile.disallowObjectPlacement = true;
+                    tile.wall.right = false;
+                    tile.passage.right = true;
+                    tile.passage.target = room2.index;
+                }
             }
         }
     }
@@ -289,7 +404,7 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
                 const [w, h] = radugen.generators.dungeons.rooms.getRoomSize();
 
                 const direction = rnd(4);
-
+                let maxDistance = 5;
                 let ox = 0, oy = 0;
 
                 let adjecentRoomInfo = this.getSize(rooms[adjecent].tiles);
@@ -297,18 +412,18 @@ radugen.generators.dungeons[radugen.generators.dungeonGenerator.Homebrew] = clas
                 switch (direction) {
                     case directions.North:
                         ox = adjecentRoomInfo.left + rnd(adjecentRoomInfo.width) - rnd(w) - 1;
-                        oy = adjecentRoomInfo.top - h - rnd(4) - 2;
+                        oy = adjecentRoomInfo.top - h - rnd(maxDistance);
                         break;
                     case directions.West:
-                        ox = adjecentRoomInfo.left - w - rnd(4) - 2;
+                        ox = adjecentRoomInfo.left - w - rnd(maxDistance);
                         oy = adjecentRoomInfo.top + rnd(adjecentRoomInfo.height) - rnd(h) - 1;
                         break;
                     case directions.South:
                         ox = adjecentRoomInfo.left + rnd(adjecentRoomInfo.width) - rnd(w) - 1;
-                        oy = adjecentRoomInfo.bottom + rnd(4) + 2;
+                        oy = adjecentRoomInfo.bottom + rnd(maxDistance);
                         break;
                     case directions.East:
-                        ox = adjecentRoomInfo.right + rnd(4) + 2;
+                        ox = adjecentRoomInfo.right + rnd(maxDistance);
                         oy = adjecentRoomInfo.top + rnd(adjecentRoomInfo.height) - rnd(h) - 1;
                         break;
                 }
