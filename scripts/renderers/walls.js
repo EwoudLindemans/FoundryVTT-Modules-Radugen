1fe769fd4ec14e8b6326482c9803131e5b766a47
Ewoud Lindemans <e.lindemans@outlook.com>
2021-03-01 01:23:16 +0100

    temp disabled new renderwalls

--- scripts/renderers/walls.js
+++ scripts/renderers/walls.js
@@ -122,18 +122,18 @@ radugen.renderer.Walls = class {
         const walls = [];
         this._grid.iterate((tile, x, y) => {
             //New wall mode
-            if (tile.wall.top) {
-                walls.push(this.getWall(x, y, 'top'));
-            }
-            if (tile.wall.left) {
-                walls.push(this.getWall(x, y, 'left'));
-            }
-            if (tile.wall.right) {
-                walls.push(this.getWall(x, y, 'right'));
-            }
-            if (tile.wall.bottom) {
-                walls.push(this.getWall(x, y, 'bottom'));
-            }
+            // if (tile.wall.top) {
+            //     walls.push(this.getWall(x, y, 'top'));
+            // }
+            // if (tile.wall.left) {
+            //     walls.push(this.getWall(x, y, 'left'));
+            // }
+            // if (tile.wall.right) {
+            //     walls.push(this.getWall(x, y, 'right'));
+            // }
+            // if (tile.wall.bottom) {
+            //     walls.push(this.getWall(x, y, 'bottom'));
+            // }
 
             //Old wall mode
             if (tile.type == 0) {
